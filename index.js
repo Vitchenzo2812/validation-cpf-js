23237dac1bd26547cb5e2e75a69446cc645bd7f3
Vitchenzo2812 <gvitchenzo@gmail.com>
2022-09-30 12:20:44 -0300

    feat: ValidationCPF with Class

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 // 705.484.450-52 070.987.720-03
 
-function ValidationCPF(cpf) {
-  Object.defineProperty(this, 'cpfCleaning', {
-    enumerable: true,
-    get: function() {
-      return cpf.replace(/\D+/g, '');
-    },
-  });
+class ValidationCPF {
+  constructor(cpf) {
+    Object.defineProperty(this, 'cpfClear', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpf.replace(/\D+/g, ''),
+    });
+  }
+
+  isSequence() {
+    return this.cpfClear.charAt(0).repeat(this.cpfClear.length) === this.cpfClear;
+  }
+
+  generatorNewCpf() {
+    const cpfWithoutDigits = this.cpfClear.slice(0, -2);
+    const firstDigit = ValidationCPF.generatorDigit(cpfWithoutDigits);
+    const secondDigit = ValidationCPF.generatorDigit(cpfWithoutDigits + firstDigit);
+    this.newCpf = cpfWithoutDigits + firstDigit + secondDigit;
+  }
+
+ static generatorDigit(abstractCpf) {
+    let total = 0;
+    let regressive = abstractCpf.length + 1;
+
+    for(let cpfArray of abstractCpf) {
+      total += regressive * Number(cpfArray);
+      regressive--;
+    }
+
+    const digit = 11 - (total % 11);
+    return digit <= 9 ? String(digit) : '0';
+  }
+
+  validation() {
+    if(!this.cpfClear) return false;
+    if(typeof this.cpfClear !== 'string') return false;
+    if(this.cpfClear.length !== 11) return false;
+    if(this.isSequence()) return false;
+    this.generatorNewCpf();
+
+    return this.newCpf === this.cpfClear;
+  }
 }
-ValidationCPF.prototype.validation = function() {
-  if (typeof this.cpfCleaning === 'undefined') return false;
-  if (this.cpfCleaning.length !== 11) return false;
-  if (this.isSequence()) return false;
-  
-  const abstractCPF = this.cpfCleaning.slice(0, -2);
-  const firstDigit = this.createDigit(abstractCPF);
-  const secondDigit = this.createDigit(abstractCPF + firstDigit);
 
-  const newCPF = abstractCPF + firstDigit + secondDigit;
-
-  return newCPF === this.cpfCleaning;
-};
-ValidationCPF.prototype.createDigit = function(abstractCPF) {
-  const CpfArray = Array.from(abstractCPF);
-  let regressive = CpfArray.length + 1;
-
-  const total = CpfArray.reduce((acc, value) => {
-    acc += (regressive * Number(value));
-    regressive--;
-    return acc;
-  }, 0);
-
-  const digit = 11 - (total % 11);
-  return digit > 9 ? '0' : String(digit);
-}
-
-ValidationCPF.prototype.isSequence = function() {
-  const sequence = this.cpfCleaning[0].repeat(this.cpfCleaning.length);
-  return sequence === this.cpfCleaning;
-}
-
-const cpf = new ValidationCPF('705.481.450-52');
+const cpf = new ValidationCPF('705.484.450-52');
 
 if(cpf.validation()) {
-  console.log('Valid CPF')
-} else {
-  console.log('Inválid CPF')
+  console.log('Válid CPF');
+}else{
+  console.log('Inválid CPF');
 }
